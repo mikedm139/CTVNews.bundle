635c26c0a319db1a6777e53068cbae0784c8d08f
Mike <mike.d.meier@gmail.com>
2012-08-29 19:39:30 -0600

    new plugin

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,41 @@
+RE_CLIPID   = Regex('clip.id = ([0-9]+)')
+RE_SUMMARY  = Regex('clip.description = escape\("(.+)"\);')
+
+CTV_URL         = 'http://www.ctvnews.ca/video'
+VIDEO_URL       = 'http://www.ctvnews.ca/video?clipId=%s&binId=%s'
+PLAYLIST_URL    = "http://www.ctvnews.ca/%s?ot=example.AjaxPageLayout.ot&pageNum=1&maxItemsPerPage=12"
+
+ART     = 'art-default.jpg'
+ICON    = 'icon-default.png'
+
+####################################################################################################
+def Start():
+    Plugin.AddPrefixHandler('/video/ctvnews', MainMenu, 'CTV News', ICON, ART)
+
+    ObjectContainer.art = R(ART)
+    ObjectContainer.title1 = 'CTV News'
+
+    DirectoryObject.thumb = R(ICON)
+
+####################################################################################################
+def MainMenu():
+    oc = ObjectContainer()
+    data = HTML.ElementFromURL(CTV_URL)
+    for section in data.xpath('//dt[@class="videoPlaylistCategories"]'):
+        section_id = section.get('id')
+        title = section.xpath('.//a')[0].text
+        oc.add(DirectoryObject(key=Callback(SectionMenu, section_title=title, section_id=section_id), title=title))
+    return oc
+
+####################################################################################################
+def SectionMenu(section_title, section_id):
+    oc = ObjectContainer(title2=section_title)
+    data = HTML.ElementFromURL(PLAYLIST_URL % section_id)
+    for article in data.xpath('//article'):
+        title = article.xpath('.//p[@class="videoPlaylistDescription"]')[0].text
+        thumb = article.xpath('.//img')[0].get('src').replace('box_180', 'landscape_960')
+        details = article.xpath('following-sibling::script')[1].text
+        clipId = RE_CLIPID.search(details).group(1)
+        summary = RE_SUMMARY.search(details).group(1)
+        oc.add(VideoClipObject(url=VIDEO_URL % (clipId, section_id), title=title, summary=summary, thumb=thumb))
+    return oc
